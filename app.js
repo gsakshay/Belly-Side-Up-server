32099e4baefe91df472215f2ae9953b143d9c66c
gsakshay <gsakshay20002002@gmail.com>
2020-06-25 22:24:14 +0530

    updated comments

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const promoRouter = require("./routes/promoRouter");
 const leaderRouter = require("./routes/leaderRouter");
 const uploadRouter = require("./routes/uploadRouter");
 const favoritesRouter = require("./routes/favoritesRouter");
+const commentRouter = require("./routes/commentRouter");
 
 
 const app = express();
@@ -63,6 +64,7 @@ app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
 app.use("/imageUpload", uploadRouter);
 app.use("/favorites", favoritesRouter);
+app.use("/comments", commentRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
